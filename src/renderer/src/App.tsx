37af6b85445d4533576328bd6678efc8eabcf0f7
elias alexandre <elliasallex@gmail.com>
2022-11-30 10:05:32 -0300

    app components

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,20 @@
 import './styles/global.css'
 
+import { Sidebar } from './components/Sidebar'
+import { Header } from './components/Header'
+
 function App(): JSX.Element {
   return (
-    <div>
-      <h1 className="text-violet-400 font-bold text-4xl">elias alexandre</h1>
+    <div className="h-screen w-screen bg-rotion-900 text-rotion-900 flex">
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+
+        <main className="flex-1 flex items-center justify-center text-rotion-400">
+          Selecione ou crie um documento
+        </main>
+      </div>
     </div>
   )
 }
